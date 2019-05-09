009bd1e786cf6cdbd472675209038988bf635a12
Tabitha Johnston <47027427+SassyData@users.noreply.github.com>
2019-05-09 16:32:58 +0100

    All pro files
    completed

---
+++ randnumr.R
@@ -0,0 +1,8 @@
+#' randnumr: Random Number Generator
+#'
+#' Easily generate random numbers from chosen distributions, and summarise the output
+#'
+#' @keywords internal
+#' @docType package
+#' @name randnumr
+"_PACKAGE"#' randnumr: Random Number Generator
